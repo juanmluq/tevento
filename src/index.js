8adea4f83b3ca977f1b5519b716a43ae692d851b
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-03-06 19:32:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './components/store';
 
-
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
